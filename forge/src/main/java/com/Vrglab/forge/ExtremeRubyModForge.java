8c20e33ea0d954e2f1716ac4727ed5609ec9d0af
Vrglab <vrglab1@gmail.com>
2024-06-15 00:25:28 +0200

    Added Some metadata

--- forge/src/main/java/com/Vrglab/forge/ExtremeRubyModForge.java
+++ forge/src/main/java/com/Vrglab/forge/ExtremeRubyModForge.java
@@ -5,13 +5,12 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import com.Vrglab.ExtremeRubyMod;
+import org.Vrglab.forge.Utils.ForgeRegistryCreator;
 
 @Mod(ExtremeRubyMod.MOD_ID)
 public final class ExtremeRubyModForge {
     public ExtremeRubyModForge() {
-
-        EventBuses.registerModEventBus(ExtremeRubyMod.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
-
+        ForgeRegistryCreator.Create(FMLJavaModLoadingContext.get().getModEventBus(), ExtremeRubyMod.MOD_ID);
         ExtremeRubyMod.init();
     }
 }
