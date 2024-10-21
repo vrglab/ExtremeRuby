8ef891ed6f45fe3cc40309b759bc50c6d0a8e369
Vrglab <vrglab1@gmail.com>
2024-10-21 13:36:21 +0200

    1.20.4: WIP: Ender tools and armours Registration code is complete

--- common/src/main/java/com/Vrglab/World/Items/ExItems.java
+++ common/src/main/java/com/Vrglab/World/Items/ExItems.java
@@ -44,6 +44,9 @@ public class ExItems {
 
         public static Map<ResourceLocation, Object> REDSTONE_TOOL_SET = createToolSet("redstone", ToolMaterials.REDSTONE, new int[]{8, 3, 10, 2, 2}, new float[]{-2f, -2.4f, -3f, -1f, -1f}, getBaseSettings());
 
+        /** ENDER **/
+
+        public static Map<ResourceLocation, Object> ENDER_TOOL_SET = createToolSet("ender", ToolMaterials.ENDER, new int[]{5, 3, 8, 2, 2}, new float[]{-2.9f, -2f, -2f, -1f, -1f}, getBaseSettings());
 
 
     /**** ARMOUR ****/
@@ -67,6 +70,10 @@ public class ExItems {
         /** REDSTONE **/
 
         public static Map<ResourceLocation, Object> REDSTONE_ARMOR_SET = createArmorSet("redstone", ArmorMaterials.REDSTONE, RedstoneArmor.class, getBaseSettings());
+
+        /** ENDER **/
+
+        public static Map<ResourceLocation, Object> ENDER_ARMOR_SET = createArmorSet("ender", ArmorMaterials.ENDER, BaseArmorClass.class, getBaseSettings());
 
 
 
@@ -181,6 +188,7 @@ public class ExItems {
         mapSet(OBSIDIAN_TOOL_SET, OBSIDIAN_ARMOR_SET, objs);
         mapSet(AMETHYST_TOOL_SET, AMETHYST_ARMOR_SET, objs);
         mapSet(REDSTONE_TOOL_SET, REDSTONE_ARMOR_SET, objs);
+        mapSet(ENDER_TOOL_SET, ENDER_ARMOR_SET, objs);
 
         return objs.toArray();
     }
