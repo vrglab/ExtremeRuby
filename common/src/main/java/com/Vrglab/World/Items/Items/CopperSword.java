2a36bd133ea7c2aada5b51c5997c86daacd4f48e
Vrglab <vrglab1@gmail.com>
2024-10-22 11:25:11 +0200

    1.20.4: WIP: Implemented Oxidation for copper tools (Un-Tested)

---
+++ common/src/main/java/com/Vrglab/World/Items/Items/CopperSword.java
@@ -0,0 +1,35 @@
+package com.Vrglab.World.Items.Items;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+
+public class CopperSword extends SwordItem {
+
+
+    public CopperSword(Tier tier, int i, float f, Properties properties) {
+        super(tier, i, f, properties);
+    }
+
+
+    @Override
+    public void inventoryTick(ItemStack itemStack, Level level, Entity entity, int i, boolean bl) {
+        super.inventoryTick(itemStack, level, entity, i, bl);
+        CopperUtils.ageCopperItemStack(itemStack, level, entity, CopperUtils.MAX_OXIDATION_STAGE);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext useOnContext) {
+        return CopperUtils.waxCopperTool(useOnContext, ()->super.useOn(useOnContext));
+    }
+
+    @Override
+    public String getDescriptionId(ItemStack stack) {
+        return CopperUtils.GetDescriptionId(stack, "sword");
+    }
+}
