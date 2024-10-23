94ff4545f8af5298dffc2f6a9ca66cb4e1f583fb
Vrglab <vrglab1@gmail.com>
2024-10-23 14:58:04 +0200

    1.20.4: WIP: Still very Jancky

--- fabric-like/src/main/java/com/Vrglab/mixin/PlayerEntityMixin.java
+++ fabric-like/src/main/java/com/Vrglab/mixin/PlayerEntityMixin.java
@@ -2,25 +2,34 @@ package com.Vrglab.mixin;
 
 
 import com.Vrglab.World.Items.Armor.InfusedElytra;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ElytraItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Player.class)
 public class PlayerEntityMixin {
 
-    @Inject(method = "tryToStartFallFlying", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "tryToStartFallFlying", at = @At("RETURN"), cancellable = true)
     private void tryToStartFallFlying(CallbackInfoReturnable<Boolean> cir) {
-         Player player = (Player)(Object)this;
-        ItemStack chestItem = player.getItemBySlot(EquipmentSlot.CHEST);
-        if (chestItem.getItem() instanceof ElytraItem && ElytraItem.isFlyEnabled(chestItem)) {
-            player.startFallFlying();
-            cir.setReturnValue(true);
+        Player player = (Player) (Object) this;
+
+        if (!cir.getReturnValue()) {
+            if (!player.onGround() && !player.isFallFlying() && !player.isInWater() && !player.hasEffect(MobEffects.LEVITATION)) {
+                ItemStack chestItem = player.getItemBySlot(EquipmentSlot.CHEST);
+
+                if (chestItem.getItem() instanceof InfusedElytra && ElytraItem.isFlyEnabled(chestItem)) {
+                    player.startFallFlying();
+                    cir.setReturnValue(true);
+                }
+            }
         }
     }
 }
