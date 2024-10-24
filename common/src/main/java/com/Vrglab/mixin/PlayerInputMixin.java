b931a940a41d1ade9d80ad047658eb795f522381
Vrglab <vrglab1@gmail.com>
2024-10-24 10:29:07 +0200

    1.20.4: WIP: Re-wrote the mixins so calls to tryToStartFallFlying get's redirected toa custom function

--- common/src/main/java/com/Vrglab/mixin/PlayerInputMixin.java
+++ common/src/main/java/com/Vrglab/mixin/PlayerInputMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.client.player.Input;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.protocol.game.ServerboundPlayerCommandPacket;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ElytraItem;
@@ -16,12 +17,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LocalPlayer.class) // or ClientPlayerEntity for older versions
 public abstract class PlayerInputMixin extends Player {
 
-    // Expose the 'input' field using @Shadow
+
     @Shadow
     private Input input;
 
@@ -37,7 +39,6 @@ public abstract class PlayerInputMixin extends Player {
     private void onAiStep(CallbackInfo ci) {
         Player player = (Player) (Object) this;
 
-
         boolean isJumping = this.input.jumping;
         boolean isFlying = this.getAbilities().flying;
 
@@ -47,10 +48,29 @@ public abstract class PlayerInputMixin extends Player {
             if ((chestItem.getItem() instanceof ElytraItem && ElytraItem.isFlyEnabled(chestItem)) ||
                     (chestItem.getItem() instanceof InfusedElytra && ElytraItem.isFlyEnabled(chestItem))) {
 
-                if (player.tryToStartFallFlying()) {
+                if (this._tryToStartFallFlying()) {
                     connection.send(new ServerboundPlayerCommandPacket(player, ServerboundPlayerCommandPacket.Action.START_FALL_FLYING));
                 }
             }
         }
+    }
+
+    @Redirect(method = "aiStep",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;tryToStartFallFlying()Z"))
+    private boolean redirectTryToStartFallFlying(LocalPlayer instance) {
+        return _tryToStartFallFlying();
+    }
+
+    private boolean _tryToStartFallFlying() {
+        Player player = (Player) (Object) this;
+        if (!player.onGround() && !player.isFallFlying() && !player.isInWater() && !player.hasEffect(MobEffects.LEVITATION)) {
+            ItemStack chestItem = player.getItemBySlot(EquipmentSlot.CHEST);
+
+            if (chestItem.getItem() instanceof ElytraItem && ElytraItem.isFlyEnabled(chestItem)) {
+                player.startFallFlying();
+                return true;
+            }
+        }
+        return false;
     }
 }
