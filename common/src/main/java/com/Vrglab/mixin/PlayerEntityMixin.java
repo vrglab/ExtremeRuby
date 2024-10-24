426a7e027a3820cb7b92a9258d7c0938ac5c6b76
Vrglab <vrglab1@gmail.com>
2024-10-24 22:48:27 +0200

    1.20.4: WIP: Mixins have failed so far

--- common/src/main/java/com/Vrglab/mixin/PlayerEntityMixin.java
+++ common/src/main/java/com/Vrglab/mixin/PlayerEntityMixin.java
@@ -1,32 +1,22 @@
 package com.Vrglab.mixin;
 
 import com.Vrglab.World.Items.Armor.InfusedElytra;
-import com.mojang.authlib.GameProfile;
-import net.minecraft.client.multiplayer.ClientPacketListener;
-import net.minecraft.client.player.Input;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.core.BlockPos;
-import net.minecraft.network.protocol.game.ServerboundPlayerCommandPacket;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ElytraItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(LocalPlayer.class) // or ClientPlayerEntity for older versions
-public abstract class PlayerInputMixin extends Player {
+@Mixin(Player.class)
+public class PlayerEntityMixin {
 
-    public PlayerInputMixin(Level level, BlockPos blockPos, float f, GameProfile gameProfile) {
-        super(level, blockPos, f, gameProfile);
-    }
-
-    @Redirect(method = "aiStep",
+    @Redirect(method = "tryToStartFallFlying",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"))
     private boolean onAiStep(ItemStack instance, Item item) {
         var test = instance.getItem() instanceof ElytraItem;
