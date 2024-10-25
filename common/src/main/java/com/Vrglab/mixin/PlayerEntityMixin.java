7ec32c167601c4ed06be9fb9e585ccb93d7a586c
Vrglab <vrglab1@gmail.com>
2024-10-25 13:01:21 +0200

    1.20.4: Refactored Mixin code to be cleaner

--- common/src/main/java/com/Vrglab/mixin/PlayerEntityMixin.java
+++ common/src/main/java/com/Vrglab/mixin/PlayerEntityMixin.java
@@ -1,17 +1,14 @@
 package com.Vrglab.mixin;
 
-import com.Vrglab.World.Items.Armor.InfusedElytra;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.EquipmentSlot;
+
+import com.Vrglab.Utils.MixinUtils;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ElytraItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Player.class)
 public class PlayerEntityMixin {
@@ -19,10 +16,6 @@ public class PlayerEntityMixin {
     @Redirect(method = "tryToStartFallFlying",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"))
     private boolean onAiStep(ItemStack instance, Item item) {
-        var test = instance.getItem() instanceof ElytraItem;
-        if (item instanceof ElytraItem) {
-            return test;
-        }
-        return instance.is(item);
+        return MixinUtils.replacedItemTypeCheck(instance, item, ElytraItem.class);
     }
 }
